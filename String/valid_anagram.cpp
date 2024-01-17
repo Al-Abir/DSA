bc611620b233a6c9faa83d7bccbf23f0398ae9f8
Al-Abir <sheikhabir196@gmail.com>
2024-01-17 12:24:43 +0600

    commit

--- String/valid_anagram.cpp
+++ String/valid_anagram.cpp
@@ -1,20 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool isAnagram(const std::string& s, const std::string& t) {
-    string sortedS = s, sortedT = t;
-    sort(sortedS.begin(), sortedS.end());
-    sort(sortedT.begin(), sortedT.end());
-    return sortedS == sortedT;
-}
-
-int main() {
-     cout <<boolalpha;  // Print bool values as true/false
-
-    // Test cases
-    cout << "Listen and Silent: " << isAnagram("listen", "silent") << std::endl;
-    cout << "Hello and World: " << isAnagram("hello", "world") << std::endl;
-
-    // Add more test cases as needed
-
+class Solution{
+   public:
+     bool anagram(string s , string t){
+         
+         if(s.length() != t.length()) return false;
+         vector <int>  h(26);
+         
+         for(int i =0; i<s.length(); i++){
+                 h[s[i]-'a']++;
+         } for(int i =0; i<t.length(); i++){
+              int x = --h[t[i]-'a'];
+              if(x<0) return false;
+         }
+         
+         return true;
+     }
+    
+    
+    
+    
+};
+int main(){
+    Solution solution;
+    string s  = "anagram";
+    string t = "nagaram";
+    
+    cout<<boolalpha<<solution.anagram(s,t)<<endl;
+    
+    
     return 0;
 }
