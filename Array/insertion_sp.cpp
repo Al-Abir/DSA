9e931c3e1b7308c7777e804b228bdd6cd842399b
Al-Abir <sheikhabir196@gmail.com>
2024-03-14 01:49:45 +0600

    Your commit message here

--- Array/insertion_sp.cpp
+++ Array/insertion_sp.cpp
@@ -2,33 +2,7 @@
 using namespace std;
 int main(){
 
-    int a[10];
-    //int n = sizeof(a)/sizeof(int);
-    //cout<<n<<endl;
-    int pos,num,size;
-    cout<<"Enter the size ";
-    cin>>size;
-    //scanf("%d",&size);
-    for(int i=0;i<size;i++){
-        cin>>a[i];
-        //scanf("%d",a[i]);
-    }
-    cout<<"Input the position ";
-    cin>>pos;
-    cout<<"Input the Number ";
-    cin>>num;
-    for(int i=size-1; i>=pos;i--){
-       a[i+1]=a[i];      
-       
-    }
-        a[pos]=num;
-   
-    cout<<"New Array\n";
-    for(int i=0;i<=size;i++){
-        cout<<a[i]<<" ";
-    }
-
-
+    cout<<"hekds";
 
     return 0;
 }
