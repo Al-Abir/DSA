f63f7ba8c0762c35fbf628e8896b528fbecab2eb
Al-Abir <sheikhabir196@gmail.com>
2024-03-14 02:21:28 +0600

    Hello

--- parttern/pratice.cpp
+++ parttern/pratice.cpp
@@ -1,78 +1,8 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// void print1(int n){
-
-//      for(int i=0; i<n; i++){
-//         for(int j=0; j<n; j++){
-//             cout<<"* ";
-//         }
-//         cout<<endl;
-//      } 
-// }
-// void print2(int n){
-//       for(int i=1; i<=n; i++){
-//          for(int j=0; j<i; j++){
-//             cout<<"* ";
-//          }
-//          cout<<endl;
-//       }
-
-// }
-// void print3(int n){
-//       for(int i=n; i>=1; i--){
-//          for(int j=0; j<=i; j++){
-//             cout<<"* ";
-//          }
-//          cout<<endl;
-//       }
-
-// }
-// void print4(int n){
-   
-//    for(int i =1;i<=2*n-1; i++)
-//    {
-//       int star =i;
-//       if(i>n) star = 2*n-i;
-//       for(int j=1; j<=star; j++){
-//          cout<<"*";
-//       }
-//       cout<<endl;
+#include<iostream>
+using namespace std;
+int mnain(){
 
 
-//    }
-
-
-// }
-// int main(){
-
-//      int t;
-//      cin>>t;
-//      for(int i=0; i<t; i++){
-//         int n;
-//         cin>>n;
-//          //print2(n);
-//           //print2(n);
-//           print4(n);
-//      }
-
-//     return 0;
-// }
-
-#include <iostream>
-
-int main() {
-    int n = 4; // Number of rows (adjust as needed)
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j <= i; ++j) {
-            if ((i + j) % 2 == 0) {
-                std::cout << "1 ";
-            } else {
-                std::cout << "0 ";
-            }
-        }
-        std::cout << "\n";
-    }
 
     return 0;
 }
